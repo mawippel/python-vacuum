8bfd229d42ef57c1a03a36409e2339b08ea76ca4
mawippel <mawippel2@hotmail.com>
2019-03-27 22:02:28 -0300

    feat: initial setup

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import matplotlib.pyplot as plt
+
+def exibir(I):
+    plt.imshow(I, 'gray')
+    plt.show(block=False)
+    plt.pause(0.5)
+    plt.clf()
+
+matrix = [[1,2,3], 
+          [1,2,3], 
+          [1,2,3]]
+
+while True:
+  exibir(matrix)
