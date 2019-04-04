6f3ad1ff8b52c5c40891711f63221928c4cabeb4
mawippel <mawippel2@hotmail.com>
2019-04-04 00:05:54 -0300

    refator: refactoring the simple agent robot

--- simple-agent-robot.py
+++ simple-agent-robot.py
@@ -7,23 +7,86 @@ def exibir(I):
     plt.pause(0.5)
     plt.clf()
 
+# 0 -> clean
+# 1 -> wall
+# 2 -> dirt
 matrix = [
     [1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 1], 
     [1, 0, 0, 0, 0, 1], 
     [1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1]]
+    [1, 1, 1, 1, 1, 1]
+]
+
+# Actions Matrix -> represents the action for each position
+# Actions = up (0), down (1), left (2), right (3), clean(4), end (5)
+actionsMatrix = [
+    [9, 9, 9, 9, 9, 9],
+    [9, 1, 3, 1, 5, 9],
+    [9, 1, 0, 1, 0, 9], 
+    [9, 1, 0, 1, 0, 9], 
+    [9, 3, 0, 3, 0, 9],
+    [9, 9, 9, 9, 9, 9]
+]
+
+# The robot always starts at matrix[1][1]
+currLine = 1
+currCol = 1
 
 def createWorld(m):
     for mI in range(1, 5):
         for aI in range(1, 5):
             number = random.randint(0, 3)
             m[mI][aI] = 2 if number == 1 else 0
-createWorld(matrix)
 
-matrix[1][1] = 3
+def findNextAction(x, y):
+  return actionsMatrix[x][y]
 
+# decides which action will be done
+# Actions = up (0), down (1), left (2), right (3), clean(4)
+def agenteReativoSimples(x, y):
+  if (matrix[x][y] == 2): # if it's dirty, return the clean action
+    return 4
+  return findNextAction(x, y)
+
+
+def main():
+  createWorld(matrix)
+  global currLine
+  global currCol
+  while True:
+    action = agenteReativoSimples(currLine, currCol)
+    if (action == 0): # go up
+      print("up")
+      currLine = currLine - 1 # remove 1 line
+    elif (action == 1): # go down
+      print("down")
+      currLine = currLine + 1
+    elif (action == 2): # go left
+      print("left")
+      currCol = currCol - 1
+    elif (action == 3): # go right
+      print("right")
+      currCol = currCol + 1
+    elif (action == 4): # clean
+      print("clean")
+      matrix[currLine][currCol] = 0
+    else:
+      print("end")
+      break
+
+  for i in range(1, len(matrix) - 1):
+    for j in range(i, len(matrix[i]) - 1):
+      print(matrix[i][j]) # verifies if the matrix is empty
+  
+
+
+
+if __name__ == "__main__":
+  main()
+
+'''
 lineBefore = 1
 columnBefore = 1
 
@@ -50,3 +113,4 @@ for i in range(1, len(matrix) - 1): #linhas
       exibir(matrix)
   
   lineBefore = i
+'''
